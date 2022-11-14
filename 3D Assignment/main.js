c838611a5d092d2408963c92303f6e2242ad0ee6
naufalalifai <naufalalifmanunited@gmail.com>
2022-11-14 10:35:13 +0700

    3D m and a little bit of i

--- 3D Assignment/main.js
+++ 3D Assignment/main.js
@@ -3,43 +3,117 @@ function main() {
     var gl = canvas.getContext("webgl");
 
     var vertices = [
-        //alphabet M
-        -0.7, -0.2, 
-        -0.4, -0.5,
-        -0.7, -0.8,
-        -0.1, -0.2,
-        -0.4, -0.5,
-        -0.1, -0.8,
-        -0.55, -0.65,
-        -0.4, -0.5,
-        -0.4, -0.8,
-        -0.25, -0.65,
+        //M - front
+        -0.7, -0.2, 0,  1, 0, 0,    0, 0, 1, //0
+        -0.4, -0.5, 0,  1, 0, 0,    0, 0, 1, //1
+        -0.7, -0.8, 0,  1, 0, 0,    0, 0, 1, //2
+        -0.1, -0.2, 0,  1, 0, 0,    0, 0, 1, //3
+        -0.4, -0.5, 0,  1, 0, 0,    0, 0, 1, //4
+        -0.1, -0.8, 0,  1, 0, 0,    0, 0, 1, //5
+        -0.55, -0.65, 0,  1, 0, 0,    0, 0, 1, //6
+        -0.4, -0.5, 0,  1, 0, 0,    0, 0, 1, //7
+        -0.4, -0.8, 0,  1, 0, 0,    0, 0, 1, //8
+        -0.25, -0.65, 0,  1, 0, 0,    0, 0, 1, //9
+        //M - back
+        -0.7, -0.2, -0.2,  1, 0, 0,    0, 0, -1, //10
+        -0.4, -0.5, -0.2,  1, 0, 0,    0, 0, -1, //11
+        -0.7, -0.8, -0.2,  1, 0, 0,    0, 0, -1, //12
+        -0.1, -0.2, -0.2,  1, 0, 0,    0, 0, -1, //13
+        -0.4, -0.5, -0.2,  1, 0, 0,    0, 0, -1, //14
+        -0.1, -0.8, -0.2,  1, 0, 0,    0, 0, -1, //15
+        -0.55, -0.65, -0.2,  1, 0, 0,    0, 0, -1, //16
+        -0.4, -0.5, -0.2,  1, 0, 0,    0, 0, -1, //17
+        -0.4, -0.8, -0.2,  1, 0, 0,    0, 0, -1, //18
+        -0.25, -0.65, -0.2,  1, 0, 0,    0, 0, -1, //19
+        //M - left
+        -0.7, -0.2, 0,  1, 0, 0,    -1, 0, 0, //20
+        -0.7, -0.8, 0,  1, 0, 0,    -1, 0, 0, //21
+        -0.7, -0.8, -0.2,  1, 0, 0,    -1, 0, 0, //22
+        -0.7, -0.2, -0.2,  1, 0, 0,    -1, 0, 0, //23
+        //M - right
+        -0.1, -0.2, 0,  1, 0, 0,    1, 0, 0, //24
+        -0.1, -0.8, 0,  1, 0, 0,    1, 0, 0, //25
+        -0.1, -0.8, -0.2,  1, 0, 0,    1, 0, 0, //26
+        -0.1, -0.2, -0.2,  1, 0, 0,    1, 0, 0, //27
+        //M - top
+        -0.7, -0.2, 0,  1, 0, 0,    0, 1, 0, //28
+        -0.4, -0.5, 0,  1, 0, 0,    0, 1, 0, //29
+        -0.4, -0.5, -0.2,  1, 0, 0,    0, 1, 0, //30
+        -0.7, -0.2, -0.2,  1, 0, 0,    0, 1, 0, //31
+        -0.1, -0.2, 0,  1, 0, 0,    0, 1, 0, //32
+        -0.1, -0.2, -0.2,  1, 0, 0,    0, 1, 0, //33
+        //M - bottom
+        -0.7, -0.8, 0,  1, 0, 0,    0, -1, 0, //34
+        -0.55, -0.65, 0,  1, 0, 0,    0, -1, 0, //35
+        -0.7, -0.8, -0.2,  1, 0, 0,    0, -1, 0, //36
+        -0.55, -0.65, -0.2,  1, 0, 0,    0, -1, 0, //37
+        -0.4, -0.8, 0,  1, 0, 0,    0, -1, 0, //38
+        -0.25, -0.65, 0,  1, 0, 0,    0, -1, 0, //39
+        -0.4, -0.8, -0.2,  1, 0, 0,    0, -1, 0, //40
+        -0.25, -0.65, -0.2,  1, 0, 0,    0, -1, 0, //41
+        -0.1, -0.8, 0,  1, 0, 0,    0, -1, 0, //42
+        -0.1, -0.8, -0.2,  1, 0, 0,    0, -1, 0, //43
 
-        //aphabet I
-        0.5, -0.8,
-        0.4, -0.7,
-        0.5, -0.6,
-        0.4, -0.5,
-        0.5, -0.4,
-        0.4, -0.3,
-        0.5, -0.2,
-        0.6, -0.3,
+        //I - front
+        0.5, -0.8, 0, 0, 1, 0,    0, 0, 1, //44
+        0.4, -0.7, 0, 0, 1, 0,    0, 0, 1, //45
+        0.5, -0.6, 0, 0, 1, 0,    0, 0, 1, //46
+        0.4, -0.5, 0, 0, 1, 0,    0, 0, 1, //47
+        0.5, -0.4, 0, 0, 1, 0,    0, 0, 1, //48
+        0.4, -0.3, 0, 0, 1, 0,    0, 0, 1, //49
+        0.5, -0.2, 0, 0, 1, 0,    0, 0, 1, //50
+        //I - back
+        0.5, -0.8, -0.2, 0, 1, 0,    0, 0, -1, //51
+        0.4, -0.7, -0.2, 0, 1, 0,    0, 0, -1, //52
+        0.5, -0.6, -0.2, 0, 1, 0,    0, 0, -1, //53
+        0.4, -0.5, -0.2, 0, 1, 0,    0, 0, -1, //54
+        0.5, -0.4, -0.2, 0, 1, 0,    0, 0, -1, //55
+        0.4, -0.3, -0.2, 0, 1, 0,    0, 0, -1, //56
+        0.5, -0.2, -0.2, 0, 1, 0,    0, 0, -1, //57
 
-        0.4, -0.1,
-        0.5, -0.05,
-        0.5, -0.15
+
     ];
 
+    var indices = [
+        //M
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 7, 8, 9,    
+        10, 11, 12, 13, 14, 15, 16, 17, 18, 17, 18, 19,
+        20, 21, 22, 20, 22, 23, 
+        24, 25, 26, 24, 26, 27,
+        28, 29, 30, 28, 30, 31, 29, 30, 32, 30, 32, 33,
+        34, 35, 36, 35, 36, 37, 35, 37, 38, 37, 38, 40, 38, 39, 40, 39, 40, 41, 39, 41, 42, 41, 42, 43,
+        //I 
+        44, 45, 46, 45, 46, 47, 46, 47, 48, 47, 48, 49, 48, 49, 50
+
+    ];
+
+    // Create a linked-list for storing the vertices data in the GPU realm
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
     // VERTEX SHADER
     var vertexShaderCode = `
-        attribute vec2 aPosition;
-        void main() {
-            gl_PointSize = 10.0;
-            gl_Position = vec4(aPosition, 0.0, 1.0);
+        attribute vec3 aPosition;
+        attribute vec3 aColor;
+        attribute vec3 aNormal;
+        uniform mat4 uModel;
+        uniform mat4 uView;
+        uniform mat4 uProjection;
+        varying vec3 vPosition;
+        varying vec3 vColor;
+        varying vec3 vNormal;
+        void main () {
+            vec4 position = vec4(aPosition, 1.0);
+            gl_Position = uProjection * uView * uModel * position;
+            // gl_Position is the final destination for storing
+            //  positional data for the rendered vertex
+            vColor = aColor;
+            vNormal = aNormal;
+            vPosition = (uModel * position).xyz;
         }
     `;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -49,8 +123,38 @@ function main() {
     // FRAGMENT SHADER
     var fragmentShaderCode = `
         precision mediump float;
+        varying vec3 vColor;
+        uniform vec3 uLightConstant;      // It represents the light color
+        uniform float uAmbientIntensity;  // It represents the light intensity
+        varying vec3 vPosition;
+        varying vec3 vNormal;
+        uniform vec3 uLightPosition;
+        uniform vec3 uViewerPosition;
+        uniform mat3 uNormalModel;
         void main() {
-            gl_FragColor = vec4(0.98, 0.88, 0.13, 1.0);
+            vec3 ambient = uLightConstant * uAmbientIntensity;
+            vec3 lightDirection = uLightPosition - vPosition;
+            vec3 normalizedLight = normalize(lightDirection);
+            vec3 normalizedNormal = normalize(uNormalModel * vNormal);
+            float cosTheta = dot(normalizedNormal, normalizedLight);
+            vec3 diffuse = vec3(0.0, 0.0, 0.0);
+            if (cosTheta > 0.0) {
+                float diffuseIntensity = cosTheta;
+                diffuse = uLightConstant * diffuseIntensity;
+            }
+            vec3 normalizedReflector = normalize(reflect(-lightDirection, normalizedNormal));
+            vec3 normalizedViewer = normalize(uViewerPosition - vPosition);
+            float cosPhi = dot(normalizedReflector, normalizedViewer);
+            vec3 specular = vec3(0., 0., 0.);
+            if (cosPhi > 0.) {
+                float shininessConstant = 100.0;    // bare minimum spec for metal
+                float specularIntensity = pow(cosPhi, shininessConstant);
+                specular = uLightConstant * specularIntensity;
+            }
+            vec3 phong = ambient + diffuse + specular;
+            gl_FragColor = vec4(phong * vColor, 1.0);
+            // gl_FragColor is the final destination for storing
+            //  color data for the rendered fragment
         }
     `;
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -63,18 +167,238 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    // Local variables
+    var isAnimated = false;
+    var theta = 0.0;
+    var direction = "";
+    var dX = 0.0;
+    var dY = 0.0;
+    // For the model (all linear transformation)
+    var uModel = gl.getUniformLocation(shaderProgram, "uModel");
+    // For the camera
+    var camera = [0.0, 0.0, 5.0];
+    var uView = gl.getUniformLocation(shaderProgram, "uView");
+    var view = glMatrix.mat4.create();  // Create an identity matrix
+    glMatrix.mat4.lookAt(
+        view,
+        camera,
+        [camera[0], 0.0, -10.0],
+        [0.0, 1.0, 0.0]
+    );
+    gl.uniformMatrix4fv(uView, false, view);
+    // For the projection
+    var uProjection = gl.getUniformLocation(shaderProgram, "uProjection");
+    var perspective = glMatrix.mat4.create();
+    glMatrix.mat4.perspective(
+        perspective,
+        Math.PI/3,  // 60 degrees
+        1.0,
+        0.5, 
+        10.0
+    );
+    gl.uniformMatrix4fv(uProjection, false, perspective);
+
+    // For the lighting and shading
+    var uLightConstant = gl.getUniformLocation(shaderProgram, "uLightConstant");
+        // Ambient
+    var uAmbientIntensity = gl.getUniformLocation(shaderProgram, "uAmbientIntensity");
+    gl.uniform3fv(uLightConstant, [1.0, 1.0, 1.0]);   // white color
+    gl.uniform1f(uAmbientIntensity, 0.4);             // 40% intensity
+        // Diffuse
+    var uLightPosition = gl.getUniformLocation(shaderProgram, "uLightPosition");
+    gl.uniform3fv(uLightPosition, [1.0, 0.0, 1.0]);
+    var uNormalModel = gl.getUniformLocation(shaderProgram, "uNormalModel");
+        // Specular
+    var uViewerPosition = gl.getUniformLocation(shaderProgram, "uViewerPosition");
+
+    // Local functions
+        // MOUSE
+    var dragging, prevx, prevy, rotation = glMatrix.mat4.create();
+    function onMouseDown (event) {
+        var x = event.clientX;
+        var y = event.clientY;
+        var rect = event.target.getBoundingClientRect();
+        if (
+            rect.left <= x &&
+            rect.right >= x &&
+            rect.top <= y &&
+            rect.bottom >= y
+        ) {
+            dragging = true;
+            prevx = x;
+            prevy = y;
+        }
+    }
+    function onMouseUp (event) {
+        dragging = false;
+    }
+    function onMouseMove (event) {
+        if (dragging) {
+            var x = event.clientX;
+            var y = event.clientY;
+            var xdiff = x - prevx;
+            var ydiff = y - prevy;
+            var inverseRotation = glMatrix.mat4.create();
+            glMatrix.mat4.invert(inverseRotation, rotation);
+            var xAxis = [1, 0, 0, 0];
+            var yAxis = [0, 1, 0, 0];
+            glMatrix.vec4.transformMat4(xAxis, xAxis, inverseRotation);
+            glMatrix.vec4.transformMat4(yAxis, yAxis, inverseRotation);
+            glMatrix.mat4.rotate(rotation, rotation, glMatrix.glMatrix.toRadian(xdiff), yAxis);
+            glMatrix.mat4.rotate(rotation, rotation, glMatrix.glMatrix.toRadian(ydiff), xAxis);
+            prevx = x;
+            prevy = y;
+        }
+    }
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mouseup", onMouseUp);
+    document.addEventListener("mousemove", onMouseMove);
+        // KEYBOARD
+    function onKeyDown (event) {
+        switch (event.keyCode) {
+            case 87: // Object UP
+                direction = "up";
+                break;
+            case 83: // Object DOWN
+                direction = "down";
+                break;
+            case 68: // Object RIGHT
+                direction = "right";
+                break;
+            case 65: // Object LEFT
+                direction = "left";
+                break;
+            case 38: // Camera UP
+                camera[1] += 0.05;
+                gl.uniform3fv(uViewerPosition, camera);
+                glMatrix.mat4.lookAt(
+                    view,
+                    camera,
+                    [camera[0], 0.0, -10.0],
+                    [0.0, 1.0, 0.0]
+                );
+                gl.uniformMatrix4fv(uView, false, view);
+                break;
+            case 40: // Camera DOWN
+                camera[1] -= 0.05;
+                gl.uniform3fv(uViewerPosition, camera);
+                glMatrix.mat4.lookAt(
+                    view,
+                    camera,
+                    [camera[0], 0.0, -10.0],
+                    [0.0, 1.0, 0.0]
+                );
+                gl.uniformMatrix4fv(uView, false, view);
+                break;
+            case 39: // Camera RIGHT
+                camera[0] += 0.05;
+                gl.uniform3fv(uViewerPosition, camera);
+                glMatrix.mat4.lookAt(
+                    view,
+                    camera,
+                    [camera[0], 0.0, -10.0],
+                    [0.0, 1.0, 0.0]
+                );
+                gl.uniformMatrix4fv(uView, false, view);
+                break;
+            case 37: // Camera LEFT
+                camera[0] -= 0.05;
+                gl.uniform3fv(uViewerPosition, camera);
+                glMatrix.mat4.lookAt(
+                    view,
+                    camera,
+                    [camera[0], 0.0, -10.0],
+                    [0.0, 1.0, 0.0]
+                );
+                gl.uniformMatrix4fv(uView, false, view);
+                break;
+            default:
+                break;
+        }
+    }
+    function onKeyUp (event) {
+        direction = "";
+    }
+    function onKeyPress (event) {
+        console.log('keypress');
+        if (event.keyCode == 32) {  // Space button
+            isAnimated = !isAnimated;
+        }
+    }
+    document.addEventListener("keypress", onKeyPress)
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+
+    // Teach the GPU how to collect
+    //  the positional values from ARRAY_BUFFER
+    //  for each vertex being processed
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(
+        aPosition, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        9 * Float32Array.BYTES_PER_ELEMENT, 
+        0);
     gl.enableVertexAttribArray(aPosition);
+    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+    gl.vertexAttribPointer(
+        aColor, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        9 * Float32Array.BYTES_PER_ELEMENT, 
+        3 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aColor);
+    var aNormal = gl.getAttribLocation(shaderProgram, "aNormal");
+    gl.vertexAttribPointer(
+        aNormal, 
+        3, 
+        gl.FLOAT, 
+        false, 
+        9 * Float32Array.BYTES_PER_ELEMENT, 
+        6 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aNormal);
+    
+    function render() {
+        gl.enable(gl.DEPTH_TEST);
+        gl.clearColor(0.0, 0.0,   0.0,  1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        var model = glMatrix.mat4.create();
+        if (isAnimated) {
+            theta += 0.01;
+        }
+        switch (direction) {
+            case "up":
+                dY += 0.1;
+                break;
+            case "down":
+                dY -= 0.1;
+                break;
+            case "left":
+                dX -= 0.1;
+                break;
+            case "right":
+                dX += 0.1;
+                break;
+        
+            default:
+                break;
+        }
+        glMatrix.mat4.translate(model, model, [dX, dY, 0.0]);
+        glMatrix.mat4.rotateZ(rotation, rotation, theta);
+        glMatrix.mat4.rotateY(rotation, rotation, theta);
+        glMatrix.mat4.multiply(model, model, rotation);
+        gl.uniformMatrix4fv(uModel, false, model);
 
-    gl.clearColor(0.85, 0.16, 0.11, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+        // For transforming the normal vector
+        var normalModel = glMatrix.mat3.create();
+        glMatrix.mat3.normalFromMat4(normalModel, model);
+        gl.uniformMatrix3fv(uNormalModel, false, normalModel);
 
-    //alphabet M
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 6, 4);
-
-    //alphabet I
-    gl.drawArrays(gl.TRIANGLE_STRIP, 10, 8);
-    gl.drawArrays(gl.TRIANGLES, 18, 3);
+        gl.drawElements(gl.TRIANGLES, indices.length, 
+            gl.UNSIGNED_SHORT, 0);
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 }
